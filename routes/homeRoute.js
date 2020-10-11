8bcd7163bb989a6eb7a841065ba90e8b33f3ba9b
Darbaz Ali <darbaz.a.h@gmail.com>
2020-10-11 12:00:27 +0300

    Create new Project

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,11 +1,31 @@
+import mongodb from 'mongodb'
+const ObjectID = mongodb.ObjectID
+
 const homeRoute = (app, projects) => {
-    app.route('/').get((req, res) => {
-        projects.find().toArray((err, result) => {
-            if (err) throw err
-            log(result)
-            res.render('index', { projects: result })
+    app.route('/')
+        .get((req, res) => {
+            projects.find().toArray((err, result) => {
+                if (err) throw err
+                log(result)
+                res.render('index', { projects: result })
+            })
         })
-    })
+
+        .post((req, res) => {
+            const { project_name } = req.body
+            // new project
+            const newProject = {
+                _id: new ObjectID(),
+                name: project_name,
+                created_on: new Date().toUTCString(),
+                issues: [],
+            }
+
+            projects.insertOne(newProject, (err, data) => {
+                if (err) throw err
+                res.redirect('/')
+            })
+        })
 }
 
 export default homeRoute
