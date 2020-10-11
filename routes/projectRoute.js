c85fa408e5a491d7d47036cf5d967161cafc7f54
Darbaz Ali <darbaz.a.h@gmail.com>
2020-10-11 13:17:08 +0300

    listing projects

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,8 +1,8 @@
-const projectRoute = (app, issues) => {
+const projectRoute = (app, projects) => {
     app.route('/:project').get((req, res) => {
         const { project } = req.params
 
-        issues.find().toArray((err, result) => {
+        projects.find().toArray((err, result) => {
             if (err) throw err
             // log(result)
             res.render('project', { name: project, data: result })
